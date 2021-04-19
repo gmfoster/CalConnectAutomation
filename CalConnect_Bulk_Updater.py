15fdd82b5299c43f22987f4eb93215a4dd7eee5f
Graham <grahammfoster96@gmail.com>
2021-04-19 12:13:44 -0700

    More updates

--- CalConnect_Bulk_Updater.py
+++ CalConnect_Bulk_Updater.py
@@ -33,8 +33,8 @@ class BulkUpdater:
 
         # Initialize webdriver
         self.driver = webdriver.Chrome(ChromeDriverManager().install(), options=self.options)
-        # if page takes longer than 150 seconds to load it times out...
-        self.driver.set_page_load_timeout(150)
+        # if page takes longer than 1500 seconds to load it times out...
+        self.driver.set_page_load_timeout(1500)
 
     def load(self):
         """
@@ -52,11 +52,11 @@ class BulkUpdater:
         # This block waits until the username/password and login fields are loaded before proceeding
         while True:
             try:
-                WebDriverWait(self.driver, 5).until(
+                WebDriverWait(self.driver, 50).until(
                     EC.presence_of_element_located((By.XPATH, "//*[@id='idToken1']")))
-                WebDriverWait(self.driver, 5).until(
+                WebDriverWait(self.driver, 50).until(
                     EC.presence_of_element_located((By.XPATH, "//*[@id='idToken2']")))
-                WebDriverWait(self.driver, 5).until(
+                WebDriverWait(self.driver, 50).until(
                     EC.presence_of_element_located((By.XPATH, "//*[@id='loginButton_0']")))
                 break
             except TimeoutException:
@@ -91,7 +91,7 @@ class BulkUpdater:
         # Wait for page to load
         while True:
             try:
-                WebDriverWait(self.driver, 5).until(
+                WebDriverWait(self.driver, 50).until(
                     EC.presence_of_element_located((By.XPATH, "//*[@id='idToken2_0']")))
                 break
             except TimeoutException:
@@ -109,7 +109,7 @@ class BulkUpdater:
         # Wait for page to load
         while True:
             try:
-                WebDriverWait(self.driver, 5).until(
+                WebDriverWait(self.driver, 50).until(
                     EC.presence_of_element_located((By.XPATH, "//*[@id='myApplication_0']/a")))
                 break
             except TimeoutException:
@@ -135,13 +135,13 @@ class BulkUpdater:
         # Wait for page to load
         while True:
             try:
-                WebDriverWait(self.driver, 5).until(
+                WebDriverWait(self.driver, 50).until(
                     EC.presence_of_element_located((By.XPATH, "//*[@id='brandBand_1']/div/div/div/div/div[2]/div/div[1]/div[2]/div[2]/div[1]/div/div/table")))
-                WebDriverWait(self.driver, 5).until(
+                WebDriverWait(self.driver, 50).until(
                     EC.presence_of_element_located((By.XPATH, "//*[@id='brandBand_1']/div/div/div/div/div[2]/div/div[1]/div[2]/div[2]/div[1]/div/div/table/tbody")))
-                WebDriverWait(self.driver, 5).until(
+                WebDriverWait(self.driver, 50).until(
                     EC.presence_of_element_located((By.TAG_NAME, "tr")))
-                WebDriverWait(self.driver, 5).until(
+                WebDriverWait(self.driver, 50).until(
                     EC.presence_of_element_located((By.TAG_NAME, "td")))
                 break
             except TimeoutException:
@@ -158,6 +158,8 @@ class BulkUpdater:
 
             # 3rd cell element (column)
             xpath = "/html/body/div[4]/div[1]/section/div[1]/div/div[2]/div[1]/div/div/div/div/div/div/div/div[2]/div/div[1]/div[2]/div[2]/div[1]/div/div/table/tbody/tr[" + str(i) + "]/td[4]"
+
+
             # Edit button in 3rd cell
             button = xpath + "/span/span[2]/button"
             # Menu produced from button click
@@ -177,7 +179,7 @@ class BulkUpdater:
             print("Edit click, waiting for menu to load")
             while True:
                 try:
-                    WebDriverWait(self.driver, 5).until(
+                    WebDriverWait(self.driver, 50).until(
                         EC.presence_of_element_located((By.XPATH, dropdown_menu)))
                     break
                 except TimeoutException:
@@ -193,8 +195,8 @@ class BulkUpdater:
             # Wait for menu elements to load
             while True:
                 try:
-                    WebDriverWait(self.driver, 5).until(
-                        EC.presence_of_element_located((By.XPATH, "/html/body/div[9]/div/ul/li[8]")))
+                    WebDriverWait(self.driver, 50).until(
+                        EC.presence_of_element_located((By.XPATH, "/html/body/div[8]")))
                     break
                 except TimeoutException:
                     print("Timeout Exception: Page elements took too long to load")
@@ -202,7 +204,7 @@ class BulkUpdater:
             # Locate and select "Closed by LHJ option"
             # IF YOU WANT TO EDIT A DIFFERENT FIELD THIS XPATH WILL MOST LIKELY NEED TO BE UPDATED #
             print("Changing selected variable")
-            closed_by_lhj = self.driver.find_element_by_xpath("/html/body/div[9]/div/ul/li[8]")
+            closed_by_lhj = self.driver.find_element_by_xpath("/html/body/div[8]/div/ul/li[8]")
             closed_by_lhj.click()
 
         # DONT UNCOMMENT THIS UNTIL YOU HAVE RAN IT ONCE AND KNOW IT IS CHANGING THE FIELD YOU WANT TO WHAT YOU WANT IT TO
@@ -227,5 +229,5 @@ if __name__ == "__main__":
     d.consent()     # consent to terms
     d.production()  # load production environment
     d.load_list()   # load list of incident id's to change and change them!
-    time.sleep(5)   # sleep to allow time to save
+    time.sleep(50)   # sleep to allow time to save
     d.close()       # close chrome
